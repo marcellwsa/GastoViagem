394118129fdfce718718f87c9e1ae014f4d23975
Marcell Wendling <mwsa.pe@gmail.com>
2024-09-25 21:28:07 -0300

    ViewBingdings e onClik dos botoes

--- app/src/main/java/devandroid/isa/gastoviagem/MainActivity.kt
+++ app/src/main/java/devandroid/isa/gastoviagem/MainActivity.kt
@@ -1,20 +1,34 @@
 package devandroid.isa.gastoviagem
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import devandroid.isa.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener(this)
+
+    }
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_calcular) {
+            calcular()
         }
+    }
+
+    private fun calcular() {
+        Toast.makeText(this, "Fui cliclado", Toast.LENGTH_SHORT).show();
     }
 }
